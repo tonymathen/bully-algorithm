edc6cd089a3c9a8246a9f6be7f1c67e288d9911f
tonymathen <tonymathen@gmail.com>
2023-06-03 02:05:31 -0700

    created basic heartbeat and receiver mechanism

--- src/BullyAlgorithm.java
+++ src/BullyAlgorithm.java
@@ -1,21 +1,20 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.HashMap;
 
 public class BullyAlgorithm implements Runnable {
     static int nodeId = -1;
     static HashMap<Integer,String> nodes= new HashMap<>();
-    static boolean isLeader = false;
-    static String operation;
-    static int leaderId = -1;
-    static int nodeServerPort = 5511;
+    String operation;
+    static int leaderId = 3;
+    static int nodeServerPort = 8070;
 
     public BullyAlgorithm(String operation) {
         this.operation = operation;
     }
+
     public static void initializeNodes() throws UnknownHostException {
         nodes.put(1,"node1");
         nodes.put(2,"node2");
@@ -41,35 +40,66 @@ public class BullyAlgorithm implements Runnable {
         }
     }
 
-    @Override
-    public void run() {
-        switch(operation) {
-            case "HEARTBEAT":
-                while(true) {
-                    try {
-                        System.out.println("");
-                        if((nodeId != leaderId)) {
-                            Thread.sleep(1500*60);
-                            String leaderNode = nodes.get(leaderId);
-                            Socket socket = new Socket(leaderNode, nodeServerPort);
-                            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                            out.writeUTF("HEARTBEAT");
-                            out.writeUTF(nodeId + " ");
-                            System.out.println("Sent heartbeat to " + leaderNode);
 
-                        }
-                    } catch (Exception e) {
-                        //Start new election
+    @Override
+    public void run(){
+        System.out.println("Inside run for "+nodeId);
+        System.out.println("Operation for "+nodeId + " is "+ operation);
+        if(operation.equals("RECEIVER")){
+            ServerSocket serverSocket = null;
+            try{
+                serverSocket = new ServerSocket(nodeServerPort);
+                while(true){
+                    Socket socket = serverSocket.accept();
+                    DataInputStream in = new DataInputStream(socket.getInputStream());
+                    String option=in.readUTF();
+                    if(option.equals("HEARTBEAT")){
+                        int sender=Integer.parseInt(in.readUTF());
+                        System.out.println("HEARTBEAT received from "+nodes.get(sender));
                     }
+                    socket.close();
                 }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+
+            }
         }
 
+        else if(operation.equals("HEARTBEAT")){
+            while(true){
+                try{
+                    System.out.print("");
+
+                        Thread.sleep(2000);
+                        String peerNode=nodes.get(leaderId);
+                        System.out.println("Destination Server "+ peerNode + " Port "+nodeServerPort);
+                        Socket socket = new Socket(peerNode, nodeServerPort);
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                        out.writeUTF("HEARTBEAT");
+                        out.writeUTF(nodeId+"");
+
+                        System.out.println("Sent HEARTBEAT to : "+peerNode);
+                }
+
+                catch(Exception e){
+                    System.out.printf("Unable to connect .. retrying");
+
+                }
+            }
+        }
     }
 
     public static void main(String[] args) throws UnknownHostException {
         initializeNodes();
-        Runnable heartbeat = new BullyAlgorithm("HEARTBEAT");
-        new Thread(heartbeat).start();
+//        System.out.println("Before Run");
+            System.out.println("Receiver for "+nodeId + " starting");
+            Runnable receiver = new BullyAlgorithm("RECEIVER");
+            new Thread(receiver).start();
+
+            System.out.println("Heartbeat from "+nodeId + " starting");
+            Runnable heartbeat = new BullyAlgorithm("HEARTBEAT");
+            new Thread(heartbeat).start();
 
         while(true) {}
 
